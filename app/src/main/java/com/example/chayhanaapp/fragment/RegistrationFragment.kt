d0935922d6a6f9001001f5840fab7576e6e342d2
WilzZiX <nodirbarotov0707@gmail.com>
2021-03-07 21:32:21 +0500

    chayhananewfragments

---
+++ app/src/main/java/com/example/chayhanaapp/fragment/RegistrationFragment.kt
@@ -0,0 +1,14 @@
+package com.example.chayhanaapp.fragment
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.chayhanaapp.R
+
+class RegistrationFragment : Fragment(R.layout.fragment_subscruption) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
