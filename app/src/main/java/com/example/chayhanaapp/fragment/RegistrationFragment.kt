d0180149cf0a67ce31dc274f5d6e7dd95ead5ee7
WilzZix <nodirbarotov0707@gmail.com>
2021-03-11 12:46:51 +0500

    'nimadir'

--- app/src/main/java/com/example/chayhanaapp/fragment/RegistrationFragment.kt
+++ app/src/main/java/com/example/chayhanaapp/fragment/RegistrationFragment.kt
@@ -7,8 +7,12 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.chayhanaapp.R
 
-class RegistrationFragment : Fragment(R.layout.fragment_subscruption) {
+class RegistrationFragment : Fragment(R.layout.fragment_11_subscribtion_payment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
+
+    //Мы отправили смс с кодом <br /> на номер +99899 ******77
+
+    //<p>Example of <br>text that is using<br>the &lt;br&gt; tag to break<br>a paragraph.</p>
 }
